558c6630e3178fce9346e0a3aaa491198f07856a
shingchunglee <shingchung.lee@student.uts.edu.au>
2024-02-26 20:25:54 +1100

    feat: add BigPowerUpState

---
+++ Assets/Scripts/Mario/BigPowerUpState.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigPowerUpState : BasePowerUpState
+{
+  public override void EnterState(PowerUpStateManager stateManager)
+  {
+  }
+
+  public override void OnTriggerEnterMushroom(PowerUpStateManager stateManager)
+  {
+  }
+
+  public override void UpdateState(PowerUpStateManager stateManager)
+  {
+  }
+}
