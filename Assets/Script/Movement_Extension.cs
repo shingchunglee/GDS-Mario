4173dd8b8ad6e35633ff2f77379d8c2d310ce747
RuviniDharmasena <126440617+RuviniDharmasena@users.noreply.github.com>
2024-02-28 11:56:28 +1100

    goomba_stomp script and dot test added to movement extension script
    adjust the player tag on the script to flatten the goomba when jumped on

--- Assets/Script/Movement_Extension.cs
+++ Assets/Script/Movement_Extension.cs
@@ -17,4 +17,9 @@ public static class Movement_Extension
      return hit.collider != null && hit.rigidbody != e_rigidbody;
    }
     
+    public static bool DotTest(this Transform transform, Transform other, Vector2 testDirection)
+    {
+        Vector2 direction = other.position - transform.position;
+        return Vector2.Dot(direction.normalized, testDirection) > 0.25f;
+    }
 }
