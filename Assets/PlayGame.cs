167588883191e435c69b5f0b58d4baba12dfd20d
RuviniDharmasena <126440617+RuviniDharmasena@users.noreply.github.com>
2024-02-29 22:55:15 +1100

    entergame script - will load next scene on build index

---
+++ Assets/PlayGame.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartGame();
+        }
+    }
+
+    void StartGame()
+    {
+        
+        int gameStartIndex = 1;
+
+        
+        SceneManager.LoadScene(gameStartIndex);
+    }
+}
