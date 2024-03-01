7e0366c6e1e68ea94576c2d812a641f964011036
shingchunglee <shingchung.lee@student.uts.edu.au>
2024-03-01 12:58:08 +1100

    feat: rainbow effect on starpower

--- Assets/Scripts/Mario/PowerUp/StarPowerUpState.cs
+++ Assets/Scripts/Mario/PowerUp/StarPowerUpState.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class StarPowerUpState : DefaultPowerUpState
@@ -8,6 +9,38 @@ public class StarPowerUpState : DefaultPowerUpState
   public override void EnterState(PowerUpStateManager stateManager)
   {
     isActive = true;
+    stateManager.TriggerCoroutine(WaitAndEnd(8f));
+    stateManager.TriggerCoroutine(RainbowSprite(stateManager.spriteRenderer, 16, 0.5f));
+  }
+
+  private IEnumerator RainbowSprite(SpriteRenderer spriteRenderer, int times, float seconds)
+  {
+    Color[] rainbowColors = new Color[]
+    {
+        Color.red,
+        Color.yellow,
+        Color.green,
+        Color.cyan,
+        Color.blue,
+        Color.magenta
+    };
+
+    for (int i = 0; i < times; i++)
+    {
+      foreach (var color in rainbowColors)
+      {
+        spriteRenderer.color = color;
+        yield return new WaitForSeconds(seconds / rainbowColors.Length);
+      }
+    }
+    spriteRenderer.color = Color.white;
+  }
+
+  IEnumerator WaitAndEnd(float waitTime)
+  {
+    yield return new WaitForSeconds(waitTime);
+    Debug.Log("Is not Invincible");
+    isActive = false;
   }
 
   public override void OnTriggerEnterMushroom(
